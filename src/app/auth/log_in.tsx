0f33c0d3a6e69de2e24f7ca6339b091242c5b90a
yushi <airmanyuu@gmail.com>
2024-02-18 10:26:03 +0900

    create router header

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,22 +1,28 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import { Link, router } from 'expo-router'
 
-import Header from '../../components/Header'
 import Button from '../../components/Button'
+
+const handlePress = (): void => {
+  // ログイン
+  router.push('/memo/list')
+}
 
 const Login = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Login</Text>
         <TextInput style={styles.input} value='Email address' />
         <TextInput style={styles.input} value='Password' />
-        <Button label='Submit' />
+        <Button label='Submit' onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>登録済みですか？</Text>
-          <TouchableOpacity>
-            <Text style={styles.footerLink}>ここから新規登録</Text>
-          </TouchableOpacity>
+          <Link href='/auth/sign_up' asChild >
+            <TouchableOpacity>
+              <Text style={styles.footerLink}>ここから新規登録</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </View>
     </View>
